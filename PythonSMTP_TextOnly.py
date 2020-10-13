d028cd24cc339b0e637b9e5220ba11315b7812bf
Surya Sekhar Datta <62787659+SuryaSekhar14@users.noreply.github.com>
2020-10-13 19:57:15 +0530

    Update PythonSMTP_TextOnly.py

--- PythonSMTP_TextOnly.py
+++ PythonSMTP_TextOnly.py
@@ -1,11 +1,11 @@
 import smtplib
 
-sender=input(str("Your Email:"))
-receiver=input(str("Reciever's Email:"))
-password=input(str("Your Password:"))
+sender=input(str("Your Email:"))         #Enter your E-mail ID
+receiver=input(str("Reciever's Email:"))    #Enter Reciever's Email ID   
+password=input(str("Your Password:"))    #Enter your Password
 
-subject=input(str("Email's Subject:"))
-data=input(str("Enter Email's Body:"))
+subject=input(str("Email's Subject:"))     #Enter E-Mail's Subject
+data=input(str("Enter Email's Body:"))     #Enter E-Mail's Body Text
 
 smtpserver=smtplib.SMTP("smtp.gmail.com",587)
 smtpserver.ehlo()
