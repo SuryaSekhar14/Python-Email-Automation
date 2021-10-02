3ca82f7748b654de17bc191bc384abae41cee36b
itisaby <arnabbibhuti01@gmail.com>
2021-10-02 22:37:21 +0530

    Added python email with mime text html

---
+++ PythonMIME_html.py
@@ -0,0 +1,53 @@
+import smtplib
+# import the smtplib library
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# me = my email address
+# you = recipient's email address
+me = "my@email.com" 
+you = "your@email.com"
+
+# Create message container - the correct MIME type is multipart/alternative.
+msg = MIMEMultipart('alternative')
+msg['Subject'] = "Link"
+msg['From'] = me
+msg['To'] = you
+
+# Create the body of the message (a plain-text and an HTML version).
+
+text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.acm.org/\nmake sure to register"
+html = """\
+<html>
+  <head></head>
+  <body>
+    <p>Hi!<br>
+       How are you?<br>
+       Here is the <a href="http://www.python.org">link</a> you wanted.
+    </p>
+  </body>
+</html>
+"""
+
+# Record the MIME types of both parts - 
+# 1. text/plain
+# 2. text/html.
+part1 = MIMEText(text, 'plain')
+part2 = MIMEText(html, 'html')
+
+# Attach parts into message container.
+# According to RFC 2046, the last part of a multipart message, in this case
+# the HTML message, is best and preferred.
+msg.attach(part1)
+msg.attach(part2)
+# Send the message via local SMTP server.
+mail = smtplib.SMTP('smtp.gmail.com', 587)
+
+mail.ehlo()
+
+mail.starttls()
+# Login credentials
+mail.login('userName', 'password')
+mail.sendmail(me, you, msg.as_string())
+mail.quit()
